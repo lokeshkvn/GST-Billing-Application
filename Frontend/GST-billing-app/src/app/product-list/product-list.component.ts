ab4f815f1d7c754d4c5275c28d86e42ba60108ef
LOKESH NANDANWAR <lokeshkvn@users.noreply.github.com>
2018-03-08 03:05:51 +0530

    refactored the dialog modal to work according to the controller

--- Frontend/GST-billing-app/src/app/product-list/product-list.component.ts
+++ Frontend/GST-billing-app/src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {GstService} from '../gst.service.ts.service'
 import {Product} from '../models/product'
 import {TableModule} from 'primeng/table';
@@ -13,9 +13,13 @@ import {ButtonModule} from 'primeng/button';
 export class ProductListComponent implements OnInit {
   productsList:Product[];
   product:Product;
+  display:boolean;
+  editedProduct;
+  @Output() displayNotify = new EventEmitter<boolean>()
   constructor(private gstService:GstService) { }
 
   ngOnInit() {
+    this.display = true;
     this.getAllProducts();
   }
 
@@ -38,5 +42,38 @@ export class ProductListComponent implements OnInit {
 
   addProduct(product:Product){
     this.getAllProducts();
+    this.displayNotify.emit(true);
+  }
+
+  editProduct() {
+    if (this.product.id) {
+      this.editedProduct = {
+        product_name: this.product.product_name,
+        product_price: this.product.product_price,
+        product_gst: this.product.product_gst
+      }
+      console.log("Edited", this.editedProduct);
+      this.gstService.editProduct(this.editedProduct, this.product.product_code).subscribe(
+        data => {
+          console.log(data);
+          this.display = false;
+          this.getAllProducts();
+        }
+      )
+    }
+    else {
+      this.addaProduct();
+    }
+  }
+
+  addaProduct() {
+    this.gstService.addProduct(this.product).subscribe(
+      data => {
+        console.log(data);
+        this.display = false;
+        // this.addProductNotify.emit(this.product)
+        this.getAllProducts();
+      }
+    )
   }
 }
